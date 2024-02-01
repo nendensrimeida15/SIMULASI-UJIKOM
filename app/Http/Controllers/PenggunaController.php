ccda68b7f5429900508ab4f3876c460fd64d00d1
spc61924 <spc61924@gmail.com>
2024-02-01 14:47:08 +0700

    updated by sandy

---
+++ app/Http/Controllers/PenggunaController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PenggunaController extends Controller
+{
+    public function index()
+    {
+        return view('Pengguna.index');
+    }
+
+    public function create()
+    {
+        //
+    }
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/template/script.blade.php
@@ -0,0 +1,47 @@
+<!--- JQuery min js --->
+<script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
+
+<!--- Bootstrap Bundle js --->
+<script src="{{ asset('assets/plugins/bootstrap/popper.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+
+<!--- Ionicons js --->
+<script src="{{ asset('assets/plugins/ionicons/ionicons.js') }}"></script>
+
+<!--- Moment js --->
+<script src="{{ asset('assets/plugins/moment/moment.js') }}"></script>
+
+<!--- JQuery sparkline js --->
+<script src="{{ asset('assets/plugins/jquery-sparkline/jquery.sparkline.min.js') }}"></script>
+
+<!--- P-scroll js --->
+<script src="{{ asset('assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/perfect-scrollbar/p-scroll.js') }}"></script>
+
+<!--- Switcher js --->
+<script src="{{ asset('assets/switcher/js/switcher.js') }}"></script>
+
+<!--- Eva-icons js --->
+<script src="{{ asset('assets/js/eva-icons.min.js') }}"></script>
+
+<!--- Sidebar js --->
+<script src="{{ asset('assets/plugins/side-menu/sidemenu.js') }}"></script>
+
+<!--- sticky js --->
+<script src="{{ asset('assets/js/sticky.js') }}"></script>
+
+<!--- Right-sidebar js --->
+<script src="{{ asset('assets/plugins/sidebar/sidebar.js') }}"></script>
+<script src="{{ asset('assets/plugins/sidebar/sidebar-custom.js') }}"></script>
+
+<!--- Index js --->
+<script src="{{ asset('assets/js/script.js') }}"></script>
+
+<!--themecolor js-->
+<script src="{{ asset('assets/js/themecolor.js') }}"></script>
+
+<!--swither-styles js-->
+<script src="{{ asset('assets/js/swither-styles.js') }}"></script>
+
+<!--- Custom js --->
+<script src="{{ asset('assets/js/custom.js') }}"></script>
